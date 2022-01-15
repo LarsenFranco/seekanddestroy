b5d59ce3e7b6b270d0f03394771f2d21eb897fe3
Franco Martinez <larsenfranco@outlook.com.ar>
2022-01-15 09:16:44 -0300

    add card

---
+++ seekanddestroy/index.js
@@ -0,0 +1,103 @@
+let server='http://localhost:5000/amigos'
+
+
+//!  Parametros de FETCH
+const get={
+    method: "GET"
+}
+const post={
+    method: "POST"
+}
+const del={
+    method: "DELETE"
+}
+
+
+//*    Funciones 
+
+//!Vaciador no funciona. No se porque
+//  const vaciador = function (){
+//   $('#search').text("");
+//   $('#amigo').text("");
+//   $('#inputDelete').text("");
+//  $('#sucess').text("");
+//  }
+
+
+const muestraAmigos = function(){    
+    
+    fetch(server,get)
+    .then(response => response.json())
+    .then(function(data)
+    {   
+        let lista = document.getElementById("lista")
+        lista.innerHTML=""        
+        for (const pers of data) {                       
+            let li = document.createElement("li");
+            li.innerText = pers.name
+            lista.appendChild(li)           
+            var cruz = document.createElement("button");
+            cruz.innerHTML=""; 
+            cruz.classList="btnDel"
+            li.appendChild(cruz)  
+                    
+            cruz.addEventListener("click", function(){
+                eliminaAmigos(pers.id)
+            })            
+        }
+
+    }
+    );   
+}
+
+
+const buscaAmigos = function(){
+    muestraAmigos();
+     fetch(server,get)
+     .then(response => response.json())
+     .then(function(data)
+     {
+     let input = document.getElementById("input");
+     let span = document.getElementById("amigo")
+     for (const per of data) {
+         if(input.value==per.id){ 
+            $('.card-header').text("Resultado:")
+            $('#card-title').text("Nombre "+per.name)
+            $('#card-id').text("Identificacion: "+per.id)
+            $('#card-age').text("Edad: "+per.age)
+            $('#card-email').text("Email: "+per.email)
+            }
+     }
+     })
+ }
+
+const eliminaAmigos = function(id){
+    fetch( `${server}/${id}`,del)
+    .then(res => res.text()) // or res.json()
+    .then(function(el) { 
+        muestraAmigos()
+        let sucess = document.getElementById("sucess");
+        sucess.innerText = "Tu amigo fue borrado con exito"
+    }                 
+    )}
+
+// * Acciones de los botones    
+
+//! Actualiza amigos
+$('#boton').click(function(){
+    muestraAmigos();
+    vaciador();
+})
+//! Buscar amigo
+$('#search').click(buscaAmigos);
+//! Eliminar amigo
+$('#delete').click(function(){ 
+    let id= document.getElementById("inputDelete").value;
+    id=parseInt(id)
+    eliminaAmigos(id);
+});
+
+
+
+
+//! Funcion llenar tarjeta
